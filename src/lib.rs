5e4b1eeb62444a06be6ec4df7926eb93f9b730df
Ruben Lapauw <Ruben.Lapauw@cs.kuleuven.be>
2017-07-08 00:31:26 +0200

    Adding flattening of And, Or, Even/Odd, Equiv

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
-
+#![feature(box_syntax, box_patterns)]
 #[macro_use]
 extern crate nom;
 
 pub mod ast;
 pub mod parser;
 pub mod printer;
+pub mod transform;
---
+++ src/transform.rs
@@ -0,0 +1,124 @@
+use ast::*;
+use printer::operators::*;
+
+impl BCSAT {
+    pub fn transform<F>(&mut self, f: &F)
+        where F: Fn(Formula) -> Formula
+    {
+        use self::Statement::*;
+        for stm in self.statements.iter_mut() {
+            match *stm {
+                Assigned(ref mut form) |
+                Defined(_, ref mut form) => transform_box(form, f),
+                _ => (),
+            }
+        }
+    }
+}
+impl Formula {
+    pub fn transform<F>(&mut self, f: &F)
+        where F: Fn(Formula) -> Formula
+    {
+        let form = ::std::mem::replace(self, Formula::Constant(Constant::False));
+        let form = f(form);
+        ::std::mem::replace(self, form);
+    }
+    pub fn t<F>(self, f: &F) -> Self
+        where F: Fn(Formula) -> Formula
+    {
+        f(self)
+    }
+    pub fn tb<F>(mut self: Box<Self>, f: &F) -> Box<Self>
+        where F: Fn(Formula) -> Formula
+    {
+        self.transform(f);
+        self
+    }
+}
+
+pub fn transform_box<F>(b: &mut Box<Formula>, f: &F)
+    where F: Fn(Formula) -> Formula
+{
+    b.transform(f)
+}
+
+pub fn join_full_assoc(f: Formula) -> Formula {
+    use self::Formula::*;
+    let red_assoc = &join_full_assoc;
+    match f {
+        Equiv(v) => accumulate(Op::Equiv, v),
+        Odd(v) => accumulate(Op::Odd, v),
+        Even(v) => accumulate(Op::Even, v),
+        And(v) => accumulate(Op::And, v),
+        Or(v) => accumulate(Op::Or, v),
+        Comment(form, com) => Comment(form.tb(red_assoc), com.clone()),
+        Imply(l, r) => Imply(l.tb(red_assoc), r.tb(red_assoc)),
+        ITE(i, t, e) => ITE(i.tb(red_assoc), t.tb(red_assoc), e.tb(red_assoc)),
+        Not(n) => Not(n.tb(red_assoc)),
+        Between(l, u, mut v) => {
+            for e in v.iter_mut() {
+                transform_box(e, red_assoc);
+            }
+            Between(l, u, v)
+        }
+        f => f.clone(),
+    }
+}
+pub fn accumulate(mut op: Op, mut check: Vec<F>) -> Formula {
+    use self::Formula::*;
+    let mut rejected = vec![];
+    while let Some(f) = check.pop() {
+        match (op, f) {
+            (Op::Or, box Or(v)) |
+            (Op::And, box And(v)) |
+            (Op::Equiv, box Equiv(v)) |
+            (Op::Even, box Odd(v)) |
+            (Op::Odd, box Odd(v)) => check.extend(v.into_iter()),
+            (Op::Odd, box Even(v)) => {
+                op = Op::Even;
+                check.extend(v.into_iter())
+            }
+            (Op::Even, box Even(v)) => {
+                op = Op::Odd;
+                check.extend(v.into_iter())
+            }
+            (_, f) => rejected.push(f.tb(&join_full_assoc)),
+        }
+    }
+    rejected.reverse();
+    op.build(rejected)
+}
+
+impl Op {
+    fn build(&self, with: Vec<F>) -> Formula {
+        use self::Op::*;
+        match *self {
+            And => Formula::And(with),
+            Or => Formula::Or(with),
+            Not => {
+                assert!(with.len() == 1);
+                let n = with.into_iter().next().unwrap();
+                Formula::Not(n)
+            }
+            Imply => {
+                assert!(with.len() == 2);
+                let mut it = with.into_iter();
+                let l = it.next().unwrap();
+                let r = it.next().unwrap();
+                Formula::Imply(l, r)
+            }
+            Equiv => Formula::Equiv(with),
+            Even => Formula::Even(with),
+            Odd => Formula::Odd(with),
+            ITE => {
+                assert!(with.len() == 3);
+                let mut it = with.into_iter();
+                let i = it.next().unwrap();
+                let t = it.next().unwrap();
+                let e = it.next().unwrap();
+                Formula::ITE(i, t, e)
+            }
+            Between(l, u) => Formula::Between(l, u, with),
+        }
+    }
+}
