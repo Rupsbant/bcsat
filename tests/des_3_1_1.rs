1085fdfa7a6387607b9978006c6aab93dd7bbfd8
Ruben Lapauw <Ruben.Lapauw@cs.kuleuven.be>
2017-07-08 00:32:42 +0200

    Printing and parsing is idempotent

--- tests/des_3_1_1.rs
+++ tests/des_3_1_1.rs
@@ -1,14 +1,15 @@
 extern crate bcsat;
 use bcsat::parser::bcsat;
-use bcsat::printer::FormulaOptions;
+use bcsat::printer::*;
 #[test]
 fn parse_des_3_1_1() {
     let f = include_bytes!("des_3_1_1.bc");
     let parsed = bcsat(f);
-    let parsed = parsed.unwrap().1;
-    println!("Parsed");
-    println!("{:?}", parsed);
-    println!("{}", FormulaOptions::default().display(&parsed));
-    assert_eq!(parsed.statements.len(), 1369);
-
+    let mut parsed = parsed.unwrap().1;
+    let mut opt = FormulaOptions::default();
+    opt.odd = InfixChainOptions::NoChain(InfixOptions::Long);
+    let printed = format!("{}", opt.display(&parsed));
+    let p2 = bcsat(printed.as_bytes());
+    let p2 = p2.unwrap().1;
+    assert_eq!(parsed.statements, p2.statements);
 }
