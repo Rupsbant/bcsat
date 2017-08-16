42ca532ad4b0a42dc74d474a443cf7675562e935
Ruben Lapauw <Ruben.Lapauw@cs.kuleuven.be>
2017-08-16 15:20:18 +0200

    Transformations have a special trait with impl for F: FnMut

--- src/transform/mod.rs
+++ src/transform/mod.rs
@@ -2,16 +2,32 @@ use ast::*;
 
 pub mod join_assoc;
 
+pub trait FormulaTransform {
+    fn exec(&mut self, f: &Formula) -> Formula;
+    fn eb(&mut self, f: &Formula) -> F {
+        From::from(self.exec(f))
+    }
+    fn ev(&mut self, f: &Vec<F>) -> Vec<F> {
+        f.iter().map(|f| self.eb(f)).collect::<Vec<F>>()
+    }
+}
+impl<F> FormulaTransform for F where
+    F: FnMut(&Formula) -> Formula {
+    fn exec(&mut self, f: &Formula) -> Formula {
+        self(f)
+    }
+}
+
 impl BCSAT {
     pub fn transform<F>(&mut self, f: &mut F)
-        where F: FnMut(&Formula) -> Formula
+        where F: FormulaTransform
     {
         for stm in self.statements.iter_mut() {
             *stm = stm.transform(f)
         }
     }
     pub fn rebuild<F>(&self, f: &mut F) -> Self
-        where F: FnMut(&Formula) -> Formula
+        where F: FormulaTransform
     {
         let statements = self.statements
             .iter()
@@ -28,13 +44,13 @@ impl BCSAT {
 }
 impl Statement {
     pub fn transform<F>(&self, f: &mut F) -> Statement
-        where F: FnMut(&Formula) -> Formula
+        where F: FormulaTransform
     {
         use self::Statement::*;
         let mut stm = self.clone();
         match stm {
             Assigned(ref mut form) |
-            Defined(_, ref mut form) => *form = Rc::from(f(form)),
+            Defined(_, ref mut form) => *form = Rc::from(f.exec(form)),
             _ => (),
         };
         stm
@@ -42,10 +58,10 @@ impl Statement {
 }
 impl Formula {
     pub fn transform<F>(&mut self, f: &mut F)
-        where F: FnMut(Formula) -> Formula
+        where F: FormulaTransform
     {
         let form = ::std::mem::replace(self, Formula::Constant(Constant::False));
-        let form = f(form);
+        let form = f.exec(&form);
         ::std::mem::replace(self, form);
     }
     pub fn t<F>(self, f: &F) -> Self
